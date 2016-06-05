f9a578af11bc7562ae5af5013149a5929de15fab
Dana Centeno <danacentenodes@gmail.com>
2016-06-05 15:28:10 -0400

    added styling to front page

--- front-page.php
+++ front-page.php
@@ -7,33 +7,29 @@
   </h2>
 </div>
 
-<main class="main">
-  <section class="about-short">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa praesentium inventore sunt, libero. Architecto nostrum dignissimos, reiciendis corporis distinctio inventore repellendus quia minus voluptas aperiam at dolorem quos doloribus.</p>
-  </section>
-  <div class="fp-container">
-    <div class="fp-content">
-      <div class="bar">
-        <p>TEST TEST TEST</p>
-        <a href=""><button>Read More</button></a>
-      </div>
+<div class="main">
+  <div class="container">
+
+    <div class="content">
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-        <h2><?php //the_title(); ?></h2>
-        <h3><?php //the_field('subtitle_text'); ?></h3>
-        <blockquote><?php //the_field('bio'); ?></blockquote>
-        <?php $image = //get_field('bio_image'); ?>
+        <h2><?php the_title(); ?></h2>
+        <h3><?php the_field('subtitle_text'); ?></h3>
+        <p><?php the_field('bio'); ?></p>
+        <?php $image = get_field('bio_image'); ?>
+
+        <!-- <pre><?php print_r($image); ?></pre> -->
         
         <img src="<?php echo $image['sizes']['square'] ?>" alt="">
 
         <?php the_content(); ?>
 
       <?php endwhile; // end the loop?>
-    </div> <!-- /.content -->
+    </div> <!-- /,content -->
 
-    <?php //get_sidebar(); ?>
+    <?php get_sidebar(); ?>
 
   </div> <!-- /.container -->
-</main> <!-- /.main -->
+</div> <!-- /.main -->
 
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 	<?php // Load Meta ?>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,700,400italic|Playfair+Display:400,900italic,400italic,900' rel='stylesheet' type='text/css'>
   <title><?php  wp_title('|', true, 'right'); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
